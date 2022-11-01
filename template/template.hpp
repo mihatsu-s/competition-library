1f71d1fec4b7ac6e29a6f307c23ce57bc1d845d5
Mihatsu <77602539+mihatsu-s@users.noreply.github.com>
2022-11-01 22:19:35 +0900

    Refactor template.hpp

--- template/template.hpp
+++ template/template.hpp
@@ -5,6 +5,7 @@
 #include <bits/stdc++.h>
 #else
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <limits>
 #include <map>
@@ -18,16 +19,45 @@
 #include <vector>
 #endif
 
-#include "../macro/util.h"
-#define rep(...) NARGS_SELECTOR(__REP, __VA_ARGS__)
-#define __REP2(i, n) for (long long i = 0ll, __##i##__end = (n); i < __##i##__end; ++i)
-#define __REP3(i, a, b) for (long long i = (a), __##i##__end = (b); i < __##i##__end; ++i)
-#define __REP4(i, a, b, c) for (long long i = (a), __##i##__end = (b), __##i##__step = (c); __##i##__step > 0ll ? i < __##i##__end : i > __##i##__end; i += __##i##__step)
-#define itr(...) __ITR(REWIND(__VA_ARGS__))
-#define __ITR(...) __PRIMITIVE_ITR(__VA_ARGS__)
-#define __PRIMITIVE_ITR(v, ...) for (auto&& __VA_ARGS__ : (v))
-#define b2e(x) (x).begin(), (x).end()
-#define e2b(x) (x).rbegin(), (x).rend()
+namespace mihatsu {
+
+namespace _internal {
+struct Init {
+    Init() {
+#ifndef DEBUG
+        std::cin.tie(0);
+        std::cout.tie(0);
+        std::ios::sync_with_stdio(0);
+#endif
+        std::cout.precision(16);
+    }
+} init;
+}  // namespace _internal
+
+template <typename T>
+using unpriority_queue = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+template <typename T, typename U>
+inline T& chmax(T& x, const U& y) {
+    return (x < y) ? (x = y) : x;
+}
+template <typename T, typename U>
+inline T& chmax(T& x, U&& y) {
+    return (x < y) ? (x = y) : x;
+}
+template <typename T, typename U>
+inline T& chmin(T& x, const U& y) {
+    return (x > y) ? (x = y) : x;
+}
+template <typename T, typename U>
+inline T& chmin(T& x, U&& y) {
+    return (x > y) ? (x = y) : x;
+}
+
+}  // namespace mihatsu
+
+#include "../util/debug_print.hpp"
+#include "../util/tuple.hpp"
 
 template <typename T>
 using V = std::vector<T>;
@@ -46,37 +76,16 @@ using dd = long double;
 const ll inf = std::numeric_limits<ll>::max() / 4;
 const char lf = '\n';
 
-namespace mihatsu {
-
-namespace _internal {
-struct Init {
-    Init() {
-#ifndef DEBUG
-        std::cin.tie(0);
-        std::cout.tie(0);
-        std::ios::sync_with_stdio(0);
-#endif
-        std::cout.precision(16);
-    }
-} init;
-}  // namespace _internal
-
-template <typename T>
-using unpriority_queue = std::priority_queue<T, V<T>, std::greater<T>>;
-
-template <typename T, typename U>
-inline T& chmax(T& x, const U& y) {
-    return (x < y) ? (x = y) : x;
-}
-template <typename T, typename U>
-inline T& chmin(T& x, const U& y) {
-    return (x > y) ? (x = y) : x;
-}
-
-}  // namespace mihatsu
-
-#include "../util/debug_print.hpp"
-#include "../util/tuple.hpp"
+#include "../macro/util.h"
+#define rep(...) NARGS_SELECTOR(__REP, __VA_ARGS__)
+#define __REP2(i, n) for (long long i = 0ll, __##i##__end = (n); i < __##i##__end; ++i)
+#define __REP3(i, a, b) for (long long i = (a), __##i##__end = (b); i < __##i##__end; ++i)
+#define __REP4(i, a, b, c) for (long long i = (a), __##i##__end = (b), __##i##__step = (c); __##i##__step > 0ll ? i < __##i##__end : i > __##i##__end; i += __##i##__step)
+#define itr(...) __ITR(REWIND(__VA_ARGS__))
+#define __ITR(...) __PRIMITIVE_ITR(__VA_ARGS__)
+#define __PRIMITIVE_ITR(v, ...) for (auto&& __VA_ARGS__ : (v))
+#define b2e(x) (x).begin(), (x).end()
+#define e2b(x) (x).rbegin(), (x).rend()
 
 using namespace std;
 using namespace mihatsu;
