446f1cf31a96444c2e3865998d921714617acb3e
Mihatsu <77602539+mihatsu-s@users.noreply.github.com>
2022-11-07 17:14:54 +0900

    Fix b2e return type

--- util/b2e.hpp
+++ util/b2e.hpp
@@ -22,55 +22,50 @@ struct b2e_impl {
         return ++counter % 2 == 1;
     }
 
-    using begin_type = decltype(std::begin(std::declval<T>()));
-    using end_type = decltype(std::end(std::declval<T>()));
-    using rbegin_type = decltype(std::rbegin(std::declval<T>()));
-    using rend_type = decltype(std::rend(std::declval<T>()));
-
-    // for lvalue
-    static inline begin_type begin(T& val) {
-        return std::begin(val);
-    }
-    static inline end_type end(T& val) {
-        return std::end(val);
-    }
-    static inline rbegin_type rbegin(T& val) {
-        return std::rbegin(val);
-    }
-    static inline rend_type rend(T& val) {
-        return std::rend(val);
-    }
-
-    // for rvalue (first call)
-    static inline begin_type begin(T&& val) {
+    // first call
+    static inline auto begin(T&& val) {
         ptr = std::make_unique<T>(std::forward<T>(val));
         return begin();
     }
-    static inline end_type end(T&& val) {
+    static inline auto end(T&& val) {
         ptr = std::make_unique<T>(std::forward<T>(val));
         return end();
     }
-    static inline rbegin_type rbegin(T&& val) {
+    static inline auto rbegin(T&& val) {
         ptr = std::make_unique<T>(std::forward<T>(val));
         return rbegin();
     }
-    static inline rend_type rend(T&& val) {
+    static inline auto rend(T&& val) {
         ptr = std::make_unique<T>(std::forward<T>(val));
         return rend();
     }
 
-    // for rvalue (second call)
-    static inline begin_type begin() {
+    // second call
+    static inline auto begin() {
         return std::begin(*ptr);
     }
-    static inline end_type end() {
+    static inline auto end() {
         return std::end(*ptr);
     }
-    static inline rbegin_type rbegin() {
+    static inline auto rbegin() {
         return std::rbegin(*ptr);
     }
-    static inline rend_type rend() {
+    static inline auto rend() {
         return std::rend(*ptr);
+    }
+
+    // not called (written only for type consistency)
+    static inline auto begin(T& val) {
+        return std::begin(val);
+    }
+    static inline auto end(T& val) {
+        return std::end(val);
+    }
+    static inline auto rbegin(T& val) {
+        return std::rbegin(val);
+    }
+    static inline auto rend(T& val) {
+        return std::rend(val);
     }
 };
 template <typename T, int id>
@@ -82,9 +77,9 @@ inline std::unique_ptr<T> b2e_impl<T, id>::ptr;
 #define e2b(val) _MIHATSU_B2E(val, rbegin, rend)
 #define _MIHATSU_B2E(val, begin_name, end_name) _MIHATSU_B2E_IMPL(val, begin_name, end_name, mihatsu::_internal::b2e_impl, std::remove_reference_t<decltype(val)>, __COUNTER__ % MIHATSU_B2E_STORE_SIZE)
 #define _MIHATSU_B2E_IMPL(val, begin_name, end_name, impl, T, id) _MIHATSU_B2E_EXPR(begin_name, val, impl, T, id), _MIHATSU_B2E_EXPR(end_name, val, impl, T, id)
-#define _MIHATSU_B2E_EXPR(name, val, impl, T, id)                  \
-    (std::is_reference_v<decltype((val))> ? impl<T, id>::name(val) \
-     : impl<T, id>::is_first()            ? impl<T, id>::name(val) \
-                                          : impl<T, id>::name())
+#define _MIHATSU_B2E_EXPR(name, val, impl, T, id)                                                         \
+    (std::is_reference_v<decltype((val))> ? std::name(val)                                                \
+     : impl<T, id>::is_first()            ? static_cast<decltype(std::name(val))>(impl<T, id>::name(val)) \
+                                          : static_cast<decltype(std::name(val))>(impl<T, id>::name()))
 
 }  // namespace mihatsu::_internal
