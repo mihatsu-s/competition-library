0e79c3514f11128113d686f53dc1bef0a2b2f5d1
Mihatsu <77602539+mihatsu-s@users.noreply.github.com>
2022-11-18 14:37:04 +0900

    [VectorPrinter] Remove lambda-mapping

--- util/vector.hpp
+++ util/vector.hpp
@@ -35,17 +35,7 @@ struct VectorPrinter {
         return (this->ptr_l != nullptr) ? *(this->ptr_l) : *(this->ptr_r);
     }
 
-    template <typename Invocable, std::enable_if_t<std::is_invocable_v<Invocable, T>>* = nullptr>
-    auto operator<<(Invocable f) {
-        using U = std::remove_reference_t<std::invoke_result_t<Invocable, T>>;
-        const Vector& vector = get_vector();
-        std::vector<U, std::allocator<U>> new_vector;
-        new_vector.reserve(vector.size());
-        for (auto&& v : vector) new_vector.push_back(std::invoke(f, v));
-        return VectorPrinter<U, std::allocator<U>>(os, std::move(new_vector));
-    }
-
-    template <typename Delimiter, std::enable_if_t<!std::is_invocable_v<Delimiter, T>>* = nullptr>
+    template <typename Delimiter>
     std::ostream& operator<<(const Delimiter& delimiter) {
         bool is_first = true;
         for (auto&& v : get_vector()) {
