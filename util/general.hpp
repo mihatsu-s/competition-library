0483361432270b1b4a9b3e3d2ffa4be394591deb
Mihatsu <77602539+mihatsu-s@users.noreply.github.com>
2022-12-08 00:32:04 +0900

    Move max/min to namespace mihatsu

--- util/general.hpp
+++ util/general.hpp
@@ -14,17 +14,13 @@ inline T& chmin(T& x, U&& y) {
     return (y < x) ? (x = std::forward<U>(y)) : x;
 }
 
+template <class T, class U, std::enable_if_t<!std::is_same_v<T, U>>* = nullptr>
+inline constexpr std::common_type_t<T, U> max(const T& t, const U& u) {
+    return t < u ? static_cast<std::common_type_t<T, U>>(u) : static_cast<std::common_type_t<T, U>>(t);
+}
+template <class T, class U, std::enable_if_t<!std::is_same_v<T, U>>* = nullptr>
+inline constexpr std::common_type_t<T, U> min(const T& t, const U& u) {
+    return t < u ? static_cast<std::common_type_t<T, U>>(t) : static_cast<std::common_type_t<T, U>>(u);
+}
+
 }  // namespace mihatsu
-
-namespace std {
-
-template <class T, class U, enable_if_t<!is_same_v<T, U>>* = nullptr>
-inline constexpr common_type_t<T, U> max(const T& t, const U& u) {
-    return t < u ? static_cast<common_type_t<T, U>>(u) : static_cast<common_type_t<T, U>>(t);
-}
-template <class T, class U, enable_if_t<!is_same_v<T, U>>* = nullptr>
-inline constexpr common_type_t<T, U> min(const T& t, const U& u) {
-    return t < u ? static_cast<common_type_t<T, U>>(t) : static_cast<common_type_t<T, U>>(u);
-}
-
-}  // namespace std
