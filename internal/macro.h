d0551c3f9229d887617f10e0b7adb446631d9112
Mihatsu <77602539+mihatsu-s@users.noreply.github.com>
2022-12-16 01:52:03 +0900

    Unify repetition macros into 'rep'

--- internal/macro.h
+++ internal/macro.h
@@ -1,23 +1,18 @@
 #pragma once
 
+#define MIHATSU_CONCAT(a, b) _MIHATSU_PRIMITIVE_CONCAT(a, b)
 #define _MIHATSU_PRIMITIVE_CONCAT(a, b) a##b
-#define _MIHATSU_CONCAT(a, b) _MIHATSU_PRIMITIVE_CONCAT(a, b)
 
+#define MIHATSU_NARGS(...) _MIHATSU_SELECT_11TH(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 #define _MIHATSU_SELECT_11TH(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, ...) _11
-#define _MIHATSU_NARGS(...) _MIHATSU_SELECT_11TH(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 
-#define MIHATSU_NARGS_SELECTOR(prefix, ...)      \
-    _MIHATSU_CONCAT(prefix, _MIHATSU_NARGS(__VA_ARGS__)) \
-    (__VA_ARGS__)
+#define MIHATSU_NARGS_SELECTOR(prefix, ...) MIHATSU_CONCAT(prefix, MIHATSU_NARGS(__VA_ARGS__))(__VA_ARGS__)
 
-#define _MIHATSU_REWIND1(z) z
-#define _MIHATSU_REWIND2(a, z) z, a
-#define _MIHATSU_REWIND3(a, b, z) z, a, b
-#define _MIHATSU_REWIND4(a, b, c, z) z, a, b, c
-#define _MIHATSU_REWIND5(a, b, c, d, z) z, a, b, c, d
-#define _MIHATSU_REWIND6(a, b, c, d, e, z) z, a, b, c, d, e
-#define _MIHATSU_REWIND7(a, b, c, d, e, f, z) z, a, b, c, d, e, f
-#define _MIHATSU_REWIND8(a, b, c, d, e, f, g, z) z, a, b, c, d, e, f, g
-#define _MIHATSU_REWIND9(a, b, c, d, e, f, g, h, z) z, a, b, c, d, e, f, g, h
-#define _MIHATSU_REWIND10(a, b, c, d, e, f, g, h, i, z) z, a, b, c, d, e, f, g, h, i
-#define MIHATSU_REWIND(...) MIHATSU_NARGS_SELECTOR(_MIHATSU_REWIND, __VA_ARGS__)
+#define MIHATSU_IS_BLANKETED(token) MIHATSU_NARGS_SELECTOR(_MIHATSU_IS_BLANKETED_IMPL, _MIHATSU_0_COMMA_0 token)
+#define _MIHATSU_0_COMMA_0(...) 0, 0
+#define _MIHATSU_IS_BLANKETED_IMPL1(...) 0
+#define _MIHATSU_IS_BLANKETED_IMPL2(...) 1
+
+#define MIHATSU_CONDITIONAL(cond, case_1, case_0) MIHATSU_CONCAT(_MIHATSU_CONDITIONAL_IMPL, cond)(case_1, case_0)
+#define _MIHATSU_CONDITIONAL_IMPL1(case_1, case_0) case_1
+#define _MIHATSU_CONDITIONAL_IMPL0(case_1, case_0) case_0
--- util/rep.hpp
+++ util/rep.hpp
@@ -1,22 +1,93 @@
 #pragma once
 
+#include <iterator>
+#include <type_traits>
 #include "../internal/macro.h"
+#include "../internal/meta.hpp"
 
-#define rep(...) MIHATSU_NARGS_SELECTOR(_MIHATSU_REP, __VA_ARGS__)
-#define _MIHATSU_REP2(i, n) for (long long i = 0ll, _##i##_end = (n); i < _##i##_end; ++i)
-#define _MIHATSU_REP3(i, a, b) for (long long i = (a), _##i##_end = (b); i < _##i##_end; ++i)
-#define _MIHATSU_REP4(i, a, b, c) for (long long i = (a), _##i##_end = (b), _##i##_step = (c); _##i##_step > 0ll ? i < _##i##_end : i > _##i##_end; i += _##i##_step)
-#define rep_rev(...) MIHATSU_NARGS_SELECTOR(_MIHATSU_REP_REV, __VA_ARGS__)
-#define _MIHATSU_REP_REV2(i, n) for (long long i = static_cast<long long>(n) - 1; i >= 0; --i)
-#define _MIHATSU_REP_REV3(i, a, b) for (long long i = static_cast<long long>(b) - 1, _##i##_end = (a); i >= _##i##_end; --i)
-#define itr(...) _MIHATSU_ITR(MIHATSU_REWIND(__VA_ARGS__))
-#define _MIHATSU_ITR(...) _MIHATSU_PRIMITIVE_ITR(__VA_ARGS__)
-#define _MIHATSU_PRIMITIVE_ITR(v, ...) for (auto&& __VA_ARGS__ : (v))
-#define itr_rev(...) _MIHATSU_ITR_REV(__COUNTER__, MIHATSU_REWIND(__VA_ARGS__))
-#define _MIHATSU_ITR_REV(...) _MIHATSU_PRIMITIVE_ITR_REV(__VA_ARGS__)
-#define _MIHATSU_PRIMITIVE_ITR_REV(id, v, ...)                                                \
-    if (auto&& _itr_rev_##id##_range = (v); true)                                             \
-        if (auto _itr_rev_##id##_begin = std::rbegin(_itr_rev_##id##_range); true)            \
-            if (auto _itr_rev_##id##_end = std::rend(_itr_rev_##id##_range); true)            \
-                for (; _itr_rev_##id##_begin != _itr_rev_##id##_end; ++_itr_rev_##id##_begin) \
-                    if (auto&& __VA_ARGS__ = *_itr_rev_##id##_begin; true)
+namespace mihatsu::_internal::rep_impl {
+
+template <class T>
+struct numeric_range {
+    T size;
+    constexpr inline numeric_range(const T& size) noexcept
+        : size(size) {}
+
+    class iterator {
+        friend numeric_range<T>;
+
+        T val;
+        constexpr inline iterator(const T& val) noexcept
+            : val(val) {}
+
+       public:
+        inline iterator& operator++() noexcept {
+            ++val;
+            return *this;
+        }
+        constexpr inline T& operator*() noexcept {
+            return val;
+        }
+        constexpr inline bool operator!=(const iterator& other) const noexcept {
+            return val < other.val;
+        }
+    };
+    constexpr inline iterator begin() const noexcept { return iterator(0); }
+    constexpr inline iterator end() const noexcept { return iterator(size); }
+
+    class reverse_iterator {
+        friend numeric_range<T>;
+
+        T val;
+        constexpr inline reverse_iterator(const T& val) noexcept
+            : val(val) {}
+
+       public:
+        inline reverse_iterator& operator++() noexcept {
+            --val;
+            return *this;
+        }
+        constexpr inline T& operator*() noexcept {
+            return val;
+        }
+        constexpr inline bool operator!=(const reverse_iterator& other) const noexcept {
+            return val >= other.val;
+        }
+    };
+    constexpr inline reverse_iterator rbegin() const noexcept { return reverse_iterator(size - 1); }
+    constexpr inline reverse_iterator rend() const noexcept { return reverse_iterator(0); }
+};
+
+template <class T, std::enable_if_t<is_container_v<std::remove_reference_t<T>>>* = nullptr>
+constexpr inline decltype(auto) range(T&& x) {
+    return x;
+}
+template <class T, std::enable_if_t<!is_container_v<std::remove_reference_t<T>>>* = nullptr>
+constexpr inline decltype(auto) range(T&& x) {
+    return numeric_range<std::remove_reference_t<T>>(x);
+}
+
+}  // namespace mihatsu::_internal::rep_impl
+
+#define rep(var, ...) MIHATSU_CONDITIONAL(MIHATSU_IS_BLANKETED(var), _MIHATSU_REP_BLANKETED, _MIHATSU_REP)(var, __VA_ARGS__)
+#define _MIHATSU_REP(...) MIHATSU_NARGS_SELECTOR(_MIHATSU_REP, __VA_ARGS__)
+#define _MIHATSU_REP2(i, n) \
+    if (auto&& _##i##_range = (n); true) _MIHATSU_REP_RANGE(i, mihatsu::_internal::rep_impl::range(_##i##_range))
+#define _MIHATSU_REP3(i, a, b) for (std::common_type_t<decltype(a), decltype(b)> i = (a), _##i##_end = (b); i != _##i##_end; ++i)
+#define _MIHATSU_REP4(i, a, b, c) for (std::common_type_t<decltype(a), decltype(b), decltype(c)> i = (a), _##i##_end = (b), _##i##_step = (c); _##i##_step > 0 ? i < _##i##_end : i > _##i##_end; i += _##i##_step)
+#define _MIHATSU_REP_BLANKETED(vars, ...) _MIHATSU_REP_RANGE(_MIHATSU_REP_BLANKETED_VARS vars, (__VA_ARGS__))
+#define _MIHATSU_REP_BLANKETED_VARS(...) [__VA_ARGS__]
+#define _MIHATSU_REP_RANGE(var, range) for (auto&& var : range)
+
+#define rep_rev(var, ...) MIHATSU_CONDITIONAL(MIHATSU_IS_BLANKETED(var), _MIHATSU_REPREV_BLANKETED, _MIHATSU_REPREV)(var, __VA_ARGS__)
+#define _MIHATSU_REPREV(...) MIHATSU_NARGS_SELECTOR(_MIHATSU_REPREV, __VA_ARGS__)
+#define _MIHATSU_REPREV2(i, n) _MIHATSU_REPREV_RANGE(__COUNTER__, mihatsu::_internal::rep_impl::range(n), i)
+#define _MIHATSU_REPREV3(i, a, b) for (std::common_type_t<decltype(a), decltype(b)> i = b - 1, _##i##_end = a - 1; i != _##i##_end; --i)
+#define _MIHATSU_REPREV_BLANKETED(vars, ...) _MIHATSU_REPREV_RANGE(__COUNTER__, (__VA_ARGS__), _MIHATSU_REP_BLANKETED_VARS vars)
+#define _MIHATSU_REPREV_RANGE(...) _MIHATSU_REPREV_RANGE_IMPL(__VA_ARGS__)
+#define _MIHATSU_REPREV_RANGE_IMPL(id, range, ...)                                      \
+    if (auto&& _reprev##id##_range = range; true)                                       \
+        if (auto _reprev##id##_begin = std::rbegin(_reprev##id##_range); true)          \
+            if (auto _reprev##id##_end = std::rend(_reprev##id##_range); true)          \
+                for (; _reprev##id##_begin != _reprev##id##_end; ++_reprev##id##_begin) \
+                    if (auto&& __VA_ARGS__ = *_reprev##id##_begin; true)
